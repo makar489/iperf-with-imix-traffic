c567c579d8ddc0372c150b902d45c1d76dc29cc6
Макар Пожидаев <makar.po@MacBook-Air-Makar.local>
2026-07-29 12:31:31 +0300

    imix: initialize IMIX traffic generation framework
    Introduce the initial IMIX (Internet Mix) traffic generation framework to
    iperf3. This foundation enables simulation of realistic network traffic
    patterns with variable packet sizes. The framework includes the necessary
    data structures and initialization routines that will support IMIX-based
    traffic generation across different transport protocols, addressing the need
    for more realistic network performance testing beyond uniform packet sizes.
    
    Signed-off-by: Makar Pozhidaev <makar.po489@mail.ru>

---
+++ src/imix.c
@@ -0,0 +1,5 @@
+//
+// Created by Макар Пожидаев on 29.07.2026.
+//
+
+#include "imix.h"
---
+++ src/imix.h
@@ -0,0 +1,8 @@
+//
+// Created by Макар Пожидаев on 29.07.2026.
+//
+
+#ifndef IPERF_WITH_IMIX_TRAFFIC_IMIX_H
+#define IPERF_WITH_IMIX_TRAFFIC_IMIX_H
+
+#endif //IPERF_WITH_IMIX_TRAFFIC_IMIX_H
